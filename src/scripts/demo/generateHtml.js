52e5a683bfb2794957b3561a00dafe3c473f7d1c
nhanbitas <nhandev.bitas@gmail.com>
2024-02-27 14:58:58 +0700

    update

--- src/scripts/demo/generateHtml.js
+++ src/scripts/demo/generateHtml.js
@@ -48,36 +48,36 @@ if (window.self == window.top) {
   // Add favicon
   const faviconLink = document.createElement("link");
   faviconLink.rel = "icon";
-  faviconLink.href = "../../../lib/favicon.ico";
+  faviconLink.href = "/doc-draft/src/lib/favicon.ico";
   const headElement = document.querySelector("head");
   headElement.appendChild(faviconLink);
 
   // Link stylesheets
-  addLinkStylesheet("../../../styles/output.css");
-  addLinkStylesheet("../../../styles/index.css");
-  addLinkStylesheet("../../../lib/highlight.js/theme.css");
+  addLinkStylesheet("/doc-draft/src/styles/output.css");
+  addLinkStylesheet("/doc-draft/src/styles/index.css");
+  addLinkStylesheet("/doc-draft/src/lib/highlight.js/theme.css");
 
   // Scripts
   // Import scripts after converting markdown to html because these scripts depend on the html content
   // Because of needed html to run all scripts, we need async script handler
   const addDocScript = async () => {
     // Convert markdown to html and layout based on data
-    await addScriptAsync("../../../lib/mdConverter/md-page.js", true);
-    await addScriptAsync("../../../scripts/demo/generateLayout.js");
+    await addScriptAsync("/doc-draft/src/lib/mdConverter/md-page.js", true);
+    await addScriptAsync("/doc-draft/src/scripts/demo/generateLayout.js");
 
     // Import highlight js, we need handle highlight before generate icon
-    await addScriptAsync("../../../lib/highlight.js/highlight.min.js");
+    await addScriptAsync("/doc-draft/src/lib/highlight.js/highlight.min.js");
 
     // Import demo code, because of the effect of demo-code.js on info-pane.js, we need async script handler
-    await addScriptAsync("../../../scripts/demo/info-pane.js");
-    await addScriptAsync("../../../scripts/demo/demo-code.js");
+    await addScriptAsync("/doc-draft/src/scripts/demo/info-pane.js");
+    await addScriptAsync("/doc-draft/src/scripts/demo/demo-code.js");
 
     // Import font after highlight to generate icon. If not, the example will svg tag instead of i tag
-    addScript("../../../lib/font-awesome/fontawesome.min.js");
-    addScript("../../../lib/font-awesome/regular.js");
+    addScript("/doc-draft/src/lib/font-awesome/fontawesome.min.js");
+    addScript("/doc-draft/src/lib/font-awesome/regular.js");
 
     // After output.js is loaded, dispatch DOMContentLoaded event for make instances
-    addScript("../../../scripts/output.js").onload = () => {
+    addScript("/doc-draft/src/scripts/output.js").onload = () => {
       document.dispatchEvent(new Event("DOMContentLoaded"));
       document.dispatchEvent(new Event("HTMLParsed"));
     };
